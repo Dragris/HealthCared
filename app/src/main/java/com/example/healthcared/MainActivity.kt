edcd0ee1f5951056b912312e2b057d10f32f9717
Fouad88 <fuoadkhouri@gmail.com>
2020-03-14 10:13:51 +0100

    MainActivity Singup1 Singup2

--- app/src/main/java/com/example/healthcared/MainActivity.kt
+++ app/src/main/java/com/example/healthcared/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.healthcared
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun login(view: View) {
+
+        val intent = Intent(this,Inicio::class.java)
+        startActivity(intent)
+    }
+
+    fun action1(view: View) {
+        val intent = Intent(this,Singup1::class.java)
+        startActivity(intent)
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/healthcared/Singup1.kt
@@ -0,0 +1,44 @@
+package com.example.healthcared
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Singup1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        var checkbox = findViewById<CheckBox>(R.id.checkBox)
+
+        checkbox.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener {
+            buttonView: CompoundButton?, isChecked: Boolean ->
+
+        })
+    }
+
+
+    fun actionterms(view: View) {
+        if(checkBox.isChecked){
+            val intent = Intent(this,Singup2::class.java)
+            startActivity(intent)
+
+        }
+        else{
+            Toast.makeText(applicationContext, "You have to accpete the terms first!", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+
+}
+
+
+
+
