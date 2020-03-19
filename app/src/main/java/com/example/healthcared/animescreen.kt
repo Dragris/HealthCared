1ef30c913015dc74da886cbb6219e0de0392e9ab
Fouad88 <fuoadkhouri@gmail.com>
2020-03-19 15:42:49 +0100

    SplashScreen

---
+++ app/src/main/java/com/example/healthcared/animescreen.kt
@@ -0,0 +1,24 @@
+package com.example.healthcared
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.coroutines.delay
+import java.lang.Exception
+
+class animescreen : AppCompatActivity() {
+lateinit var handler : Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animescreen)
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this,LogIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
+    }
+}
