36689ea1785989b3a388e3cec7e374be6cfded2b
Dragris <dragris1234@gmail.com>
2020-03-25 15:16:10 +0100

    [T1 Cl] Added controler class. Implemented lists of Rutina and Dieta. Declared load and save functions.

---
+++ app/src/main/java/com/example/healthcared/Controlador.kt
@@ -0,0 +1,19 @@
+package com.example.healthcared
+
+class Controlador {
+    var listaDieta: MutableCollection<Dieta>? =null
+    var listaRutina: MutableCollection<Rutina>? =null
+
+    //var usuario: Usuario  //En caso de ser necesario mantener qué usuario es el que está conectado
+
+    fun cargarDatos(){
+        TODO()
+        //Implementación de una carga de datos a las listas cuando se haga login
+        //Puede ser init de la clase
+    }
+
+    fun guardarDatos(){
+        TODO()
+        //Guardar datos en firebase para el usuario, borrarlos del dispositivo.
+    }
+}
