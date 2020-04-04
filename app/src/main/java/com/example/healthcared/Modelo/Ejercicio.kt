508fd652c501af06bdb892316fbbbc2f5843950c
daload <dlopezad7@alumnes.ub.edu>
2020-04-04 17:50:03 +0200

    - T3

--- app/src/main/java/com/example/healthcared/Modelo/Ejercicio.kt
+++ app/src/main/java/com/example/healthcared/Modelo/Ejercicio.kt
@@ -7,9 +7,54 @@ import kotlin.math.round
  * body indica la zona (superior, inferior, troncal) que ejercita el ejercicio.
  * repeatFactor es un factor añadido para calcular las repeticiones que harán falta por serie.
  */
-class Ejercicio(val type: Int, val body: String, val ExerciceName:String, val youtubeLink: String, val repeatFactor: Float) {
+class Ejercicio(val type: Int, val body: String, val ExerciceName:String, val youtubeLink: String, val diff: Int) {
 
-    fun getRepeat(level: Int): Int {
-        return round(repeatFactor*5.0*level).toInt()
-    }
+
 }
+
+/*
+Type
+Flexiones = 7
+Sentadillas = 15
+Sentadillas + salto = 1
+Bench Dip = 5
+Flexiones de Triceps = 5
+Plank = 7
+Leg Rise = 5
+Crunch = 7
+Jumping Jacks = 10
+Elevaciones de cadera = 2
+Mountain Climbers = 8
+Burpee =  8
+
+
+
+- Ejercicio (Dificultad)
+Tipo 1 (Ganar Fuerza)
+- Flexiones (1)
+- Sentadillas (1)
+- Sentadillas + Salto (2)
+- Bench Dip (Triceps) (1)
+- Flexiones de Triceps (2)
+- Abdominales:
+    Plank (2), Elevaciones de piernas (2), Crunch (1)
+
+Tipo 2 (Bajar de Peso)
+- Jumping Jacks (1)
+- Sentadillas (1)
+- Flexiones (1)
+- Elevaciones de cadera (1)
+- Crunch (1)
+- Plank (2)
+
+Tipo 3 (Hipertrofia)
+- Flexiones (1)
+- Sentadillas (1) (Con peso si puede ser (2))
+- Abdominales:
+    Plank (2), Elevaciones de piernas (2), Crunch (1)
+- Bench Dip (Triceps) (1)
+- Flexiones de Triceps (2)
+
+Tipo 4 (Cardio)
+- Jumping Jacks (1)
+- Sentad
--- app/src/main/java/com/example/healthcared/Modelo/Rutina.kt
+++ app/src/main/java/com/example/healthcared/Modelo/Rutina.kt
@@ -3,7 +3,31 @@ package com.example.healthcared.Modelo
 class Rutina(val rutinaName: String) {
     val lista: MutableCollection<Ejercicio>? = null
 
-    fun createRutina(type: String?){
+
+    fun createRutina(type: Int, habilidad: Int){
         TODO()//Crear lógica de selección de ejercicios
     }
+
+
+    /* Crear Rutina pautas:
+    Bajar Peso:
+        - 3 series
+        - 10-15 reps
+        - 60s de rest
+
+    Ganar Fuerza:
+        - 4-5 series
+        - 5-6 reps (max intensity)
+        - 120s de rest
+
+    Ponerse mamadisimo:
+        - 4 series
+        - 8-12 reps
+        - 90s de rest
+
+    Cardio/Tonificar:
+        - Repetir series de ejercicios durante x mins
+        - Series de 10-20 reps
+        - Max intensity
+     */
 }
